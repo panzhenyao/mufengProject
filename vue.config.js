e7e0a70c536a5e96b70ef56be27808df15e3fec2
pzy <398603688@qq.com>
2019-07-20 18:38:44 +0800

    以后有空配置cdn资源

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,22 @@
 module.exports = {
-    chainWebpack: config => {
-        // 发布模式
-        config.when(process.env.NODE_ENV === 'production', config => {
-            config.entry('app').clear().add('./src/mian-prod.js')
-            //使用externals设置排除项
-            config.set('externals', {
-                vue: 'Vue',
-                'vue-router': 'VueRouter',
-                lodash: '_',
-            })
-            //使用插件
-            config.plugin('html').tap(args => {
-                //添加参数isProd
-                args[0].isProd = false
-                return args
-            })
-        })
-        // 开发模式
-        config.when(process.env.NODE_ENV === 'development', config => {
-            config.entry('app').clear().add('./src/mian-dev.js')
-
-            //使用插件
-            config.plugin('html').tap(args=>{
-                //添加参数isProd
-                args[0].isProd = true
-                return args
-            })
-        })
-    },
-    devServer: {
-        open: true, //自动打开浏览器
-        port: 8878 //端口号
-    }
+  chainWebpack: config => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/mian-prod.js')
+    })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config
+        .entry('app')
+        .clear()
+        .add('./src/mian-dev.js')
+    })
+  },
+  devServer: {
+    open: true, //自动打开浏览器
+    port: 8878 //端口号
+  }
 }
